6fe63ec7eafa14cc0a9fa3d2f7bc2a73aea7728f
Sarah Lindmar <srhcrete@gmail.com>
2017-12-08 09:32:01 -0800

    add validation for order item quantity

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -3,9 +3,14 @@ class OrderItemsController < ApplicationController
   def create
     @order = current_order
     @item = @order.order_items.new(item_params)
-    @order.save
-    session[:order_id] = @order.id
-    redirect_to products_path
+    if @order.save
+      session[:order_id] = @order.id
+        flash[:notice] = 'Product has been added to your order.'
+      redirect_to products_path
+    else
+      flash[:notice] = 'Please add items, quantity must be filled out.'
+      redirect_to products_path
+    end    
   end
 
   def update
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,15 +2,21 @@ class Order < ApplicationRecord
   has_many :order_items
   belongs_to :user, optional: true
 
-  before_save :calculate_total
+  before_save :update_total
 
   def calculate_total
-    self.total_price = order_items.collect { |item| item.product.price * item.quantity }.sum
+    self.order_items.collect { |item| item.product.price * item.quantity }.sum
   end
 
   def finalize(user)
     self.user_id = user.id
     self.status = 2
     self.save
+  end
+
+  private
+
+  def update_total
+    self.total_price = calculate_total
   end
 end
