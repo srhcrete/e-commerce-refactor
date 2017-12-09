dadfc1b9a6da9620d007536c61814063d532fee2
Sarah Lindmar <srhcrete@gmail.com>
2017-12-08 21:21:24 -0800

    add admin to seed

--- app/helpers/cart_helper.rb
+++ app/helpers/cart_helper.rb
@@ -1,2 +1,5 @@
 module CartHelper
+  def pretty_amount(amount_in_cents)
+    number_to_currency(amount_in_cents)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@ class Seed
   def self.begin
     seed = Seed.new
     seed.generate_products
+    seed.generate_admin
   end
 
   def generate_products
@@ -10,6 +11,15 @@ class Seed
       Product.create!(name: Faker::Lorem.word, price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop)
     end
   end
+
+  def generate_admin
+    User.create(username: "aGuy",
+            email: "test@gmail.com",
+            password: "password",
+            is_female: true,
+            date_of_birth: Date.new,
+            admin: true)
+  do
 end
 
 Seed.begin
