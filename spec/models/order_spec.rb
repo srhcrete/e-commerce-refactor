98ec4f9dc562049e43560c7b6c0f6039eeec8796
Sarah Lindmar <srhcrete@gmail.com>
2017-12-15 22:30:32 -0800

    pass tests for order associations

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -4,3 +4,8 @@ describe Order, 'validation' do
   it { should validate_presence_of :total_price }
   it { should validate_presence_of :status }
 end
+
+describe Order, 'association' do
+  it { should have_many :order_items }
+  it { should belong_to :user }
+end
