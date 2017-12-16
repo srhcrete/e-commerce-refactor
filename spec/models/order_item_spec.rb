ea4dcda379883b9115be3e6517f80ec4a340e2e5
Sarah Lindmar <srhcrete@gmail.com>
2017-12-15 22:23:08 -0800

    pass test for order item association

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -4,3 +4,8 @@ describe OrderItem, 'validation' do
   it { should validate_presence_of :quantity}
   it { should validate_presence_of :product_id }
 end
+
+describe OrderItem, 'association' do
+  it { should belong_to :order }
+  it { should belong_to :product }
+end
