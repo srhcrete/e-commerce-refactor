db3b7957762781cc244003ce168f454cc885877e
Sarah Lindmar <srhcrete@gmail.com>
2017-12-15 22:39:35 -0800

    pass user column-specification tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,3 +10,12 @@ end
 describe User, 'association' do
   it { should have_many :orders }
 end
+
+describe User, 'column_specification' do
+  it { should have_db_column(:email).of_type(:string) }
+  it { should have_db_column(:name).of_type(:string) }
+  it { should have_db_column(:password_digest).of_type(:string) }
+  it { should have_db_column(:admin).of_type(:boolean) }
+  it { should have_db_column(:created_at).of_type(:datetime) }
+  it { should have_db_column(:updated_at).of_type(:datetime) }
+end
