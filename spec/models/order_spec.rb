cdb23c51c78c954143bd2619ada717dfec962fae
Sarah Lindmar <srhcrete@gmail.com>
2017-12-15 22:32:13 -0800

    pass tests for order column specification

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -9,3 +9,11 @@ describe Order, 'association' do
   it { should have_many :order_items }
   it { should belong_to :user }
 end
+
+describe Order, 'column_specification' do
+  it { should have_db_column(:user_id).of_type(:integer) }
+  it { should have_db_column(:status).of_type(:integer) }
+  it { should have_db_column(:total_price).of_type(:decimal) }
+  it { should have_db_column(:created_at).of_type(:datetime) }
+  it { should have_db_column(:updated_at).of_type(:datetime) }
+end
